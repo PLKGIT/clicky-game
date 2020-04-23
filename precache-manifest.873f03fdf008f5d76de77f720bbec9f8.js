c6881a3bb3ea9b8d90a08df0b1e4b7dc8264067f
Lammie <plkelly@yahoo.com>
2020-04-22 23:24:08 -0700

    Updates

---
+++ precache-manifest.873f03fdf008f5d76de77f720bbec9f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24edc012fb7e35d8ce722f69dcf410e6",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9884afff540986c3c9c3",
+    "url": "/clicky-game/static/css/main.08f4981d.chunk.css"
+  },
+  {
+    "revision": "c08c29776043b4fc4ac6",
+    "url": "/clicky-game/static/js/2.73b3a5ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.73b3a5ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9884afff540986c3c9c3",
+    "url": "/clicky-game/static/js/main.0b20c061.chunk.js"
+  },
+  {
+    "revision": "9025a9c5d0326b1623d9",
+    "url": "/clicky-game/static/js/runtime-main.ec6097ba.js"
+  }
+]);
